a25b9b47084db3ed18f4cd747a8449bb444d3efd
KAMOLESH MOHANTA <kamoleshmohanto677819@gmail.com>
2025-09-15 22:08:34 +0600

    Add files via upload

---
+++ sacript.js
@@ -0,0 +1,12 @@
+let map = new Map();
+
+map.set('name', 'John');
+map.set(1, 'One');
+map.set(true, 'True');
+
+console.log(map.name('name'));
+
+
+// map.forEach((value, key) => {
+//   console.log(`${key} = ${value}`);
+// });
